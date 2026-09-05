b4de10624112b6e7f45b2300381202e1bf046080
jmendez04 <isaacmendezdiaz2002@gmail.com>
2026-09-04 21:14:20 -0600

    Agregar formulario y recepcion de datos de proveedores

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def inicio():
     return render_template("inicio.html")
+
 
 @app.route("/clientes", methods=["GET", "POST"])
 def clientes():
@@ -25,6 +27,30 @@ def clientes():
         )
 
     return render_template("clientes.html")
+
+
+@app.route("/proveedores", methods=["GET", "POST"])
+def proveedores():
+    if request.method == "POST":
+        empresa = request.form["empresa"]
+        contacto = request.form["contacto"]
+        nit = request.form["nit"]
+        tipo = request.form["tipo"]
+        condicion = request.form["condicion"]
+        activo = "Sí" if "activo" in request.form else "No"
+
+        return render_template(
+            "proveedores_confirmacion.html",
+            empresa=empresa,
+            contacto=contacto,
+            nit=nit,
+            tipo=tipo,
+            condicion=condicion,
+            activo=activo
+        )
+
+    return render_template("proveedores.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
